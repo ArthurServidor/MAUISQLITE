5c41f57a402ac31545bbc81813dabe07a9a3ca9a
Arthur Servidor <86123199+ArthurServidor@users.noreply.github.com>
2025-10-20 08:38:59 -0300

    muda readme

--- MauiAppMinhaMinhasCompras/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiAppMinhaMinhasCompras.AssemblyInfo.cs
+++ MauiAppMinhaMinhasCompras/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiAppMinhaMinhasCompras.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiAppMinhaMinhasCompras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c3d0c3dc171019936a660952da5f929696147617")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiAppMinhaMinhasCompras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiAppMinhaMinhasCompras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
